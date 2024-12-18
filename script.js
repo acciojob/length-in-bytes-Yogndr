af8211702b79ac16ececc8c1c071fadaa0cccdfe
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-18 19:53:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const byteSize = (str) => {
-	return str.length;
+	let Blob=new Blob([str]);
+
+	return Blob.size;
 	// 
   // write your code here
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+// const str = prompt("Enter some string.");
 alert(byteSize(str));
